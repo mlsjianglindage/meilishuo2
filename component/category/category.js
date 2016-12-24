9036d1040089ece37632f937c4304407b413d5fb
jianglin <57358333@qq.com>
2016-12-24 10:43:28 +0800

    'hhhhhhhh'

--- component/category/category.js
+++ component/category/category.js
@@ -8,7 +8,7 @@ angular.module('categoryModule',[])
 	.service('dataCategory2',['$http',function($http){
 		return $http.get('resource/category/3.json');
 	}])
-<<<<<<< HEAD
+
 .controller('categoryCtrl',['$scope','dataCategory','dataCategory1','dataCategory2',function($scope,dataCategory,dataCategory1,dataCategory2){
 	dataCategory.success(function(res){
 		$scope.arr = res.data[15325].list;
@@ -28,24 +28,3 @@ angular.module('categoryModule',[])
 		// console.log(res.data);
 	})
 }])
-=======
-	.controller('categoryCtrl',['$scope','dataCategory','dataCategory1','dataCategory2',function($scope,dataCategory,dataCategory1,dataCategory2){
-		dataCategory.success(function(res){
-			$scope.arr = res.data[15325].list;
-			$scope.arr1 = res.data[17118].list;
-			// console.log(res.data);
-		})
-		dataCategory1.success(function(res){
-			$scope.arr2 = res.data[17124].list;
-			$scope.arr3 = res.data[17126].list;
-			$scope.arr4 = res.data[17127].list;
-			$scope.arr5 = res.data[17128].list;
-			$scope.arr6 = res.data[17129].list;
-			// console.log(res.data[17124].list);
-		})
-		dataCategory2.success(function(res){
-			$scope.arr7 = res.data[17130].list;
-			// console.log(res.data);
-		})
-	}])
->>>>>>> jianglin
