b5fb257887e006103d7986505df8db7355549855
hulu <514045368@qq.com>
2016-12-28 17:50:55 +0800

    'gengxin'

--- component/mine/address/adduseraddress/adduseraddress.js
+++ component/mine/address/adduseraddress/adduseraddress.js
@@ -1,5 +1,69 @@
-angular.module('adduseraddressModule',[])
-.controller('adduseraddressCtrl',['$scope',function($scope){
+angular.module('adduseraddressModule', [])
 
+.controller('adduseraddressCtrl', ['$scope', '$http', function($scope, $http) {
 	
-}])
+
+	//省级联动
+	$scope.error = {};
+	$scope.list = [];
+	$http.get('resource/1.json').success(function(data) {
+		$scope.list = data;
+	});
+
+	$scope.selected = '';
+	$scope.visible = false;
+	$scope.c = function() {
+		$scope.visible2 = false;
+		$scope.visible3 = false;
+//		console.log($scope.selected.name);
+		if($scope.selected==''){
+			$scope.visible = false;
+		}else{
+			$scope.visible = true;
+		}
+	};
+	
+	
+	$scope.c2 = function() {
+		$scope.visible3 = false;
+//		console.log($scope.selected2.name);
+		$scope.visible2 = true;
+	};
+	
+	
+	$scope.c3 = function() {
+//		console.log($scope.selected3.value);
+		$scope.visible3 = true;
+	};
+//	localStorage.removeItem('addressDetail');
+	
+	//存值
+	var id=0;
+	$scope.saveInfo=function(n,p,sn,s2n,s3v,d,m){
+
+		id++;console.log(id);
+		//取值
+		$scope.arrArray=JSON.parse(localStorage.getItem('addressDetailInfo'));
+		if(!$scope.arrArray){
+			$scope.arrArray=[];
+		}
+		//将值存入数组
+		
+		var objs={
+			name:n,
+			phone:p,
+			province:sn,
+			city:s2n,
+			area:s3v,
+			detail:d,
+			mail:m,
+			id:id
+		}
+		$scope.arrArray.push(objs);
+		//存值
+		localStorage.setItem('addressDetailInfo',JSON.stringify($scope.arrArray));
+		console.log($scope.arrArray);
+	}
+	
+	
+}]);
--- component/mine/mine.js
+++ component/mine/mine.js
@@ -1,24 +1,50 @@
 angular.module('mineModule',[])
-	.controller('mineCtrl',['$scope',function($scope){
+.controller('mineCtrl',['$scope','$window',function($scope,$window){
 
-		$scope.changeThisStyle = function(v) {
-			var llis = document.getElementsByTagName("li");
-			for(var i = 0; i < llis.length; i++) {
-				var lli = llis[i];
-				if(lli == document.getElementById("tab" + v)) {
-					lli.style.color= "#FF7A9A";
-				} else {
-					lli.style.color = "#666";
-				}
-			}
-			var divs = document.getElementsByClassName("tab_css");
-			for(var i = 0; i < divs.length; i++) {
-				var divV = divs[i];
-				if(divV == document.getElementById("tab" + v + "_content")) {
-					divV.style.display = "block";
-				} else {
-					divV.style.display = "none";
-				}
-			}
-		}
-	}])
+	//切换样式
+	$scope.changeThisStyle = function(v) { 
+        var llis = document.getElementsByTagName("li");  
+       	for(var i = 0; i < llis.length; i++) {  
+           	var lli = llis[i];  
+            if(lli == document.getElementById("tab" + v)) {  
+                lli.style.color= "#FF7A9A";
+            } else {  
+                lli.style.color = "#666";
+            }  
+        }
+        var divs = document.getElementsByClassName("tab_css");  
+        for(var i = 0; i < divs.length; i++) {  
+		  	var divV = divs[i];
+		  	if(divV == document.getElementById("tab" + v + "_content")) {
+				divV.style.display = "block";
+		    } else {
+				divV.style.display = "none";
+		    }  
+		}  
+    } 
+    
+}])
+
+//固定导航 (事件触发不了)
+   .directive('scrollBox',function(){
+    	return {
+    		restrict:'A',
+    		replace:true,
+    		scope:{}, // -> 独立作用域
+    		link:function(scope,element,attrs){
+    			var domOffsetTop = element[0].offsetTop; // 元素到顶部的距离
+    			var scrollTop = 0;  // 定义滚动条高度初始值
+    			window.onscroll = function(){
+    				scrollTop = document.documentElement.scrollTop || document.body.scrollTop ; 
+    				if( scrollTop >= domOffsetTop){ 	//当滚动条高度大于等于DOM节点到document顶部的距离 
+    					element.addClass('fixednav');	
+    				}else{		
+    					element.removeClass('fixednav');
+    				}
+    				console.log('滚动条高度：'+scrollTop,'DOM节点高度：'+domOffsetTop);
+    			}
+    		}
+    	}
+    })
+
+
